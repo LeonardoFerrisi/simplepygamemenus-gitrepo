5a90e0bfee9563f48fc2ef1880e3bd2533bf32e6
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2022-11-11 09:18:23 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ with open("README.md", "r") as fh:
   
 setuptools.setup(
     name="simplepygamemenus",
-    version="0.0.3",
+    version="0.0.6",
     author="Leonardo Ferrisi",
     author_email="ferrisil@union.edu",
     packages=find_packages(),
+    package_data={'simplepygamemenus': ['rect.png', 'font.ttf', 'menu.py']},
+    include_package_data=True,
     description="A package for making simple menus for your python games!",
     long_description=description,
     long_description_content_type="text/markdown",
     url="https://github.com/LeonardoFerrisi/simplepygamemenus-gitrepo",
     license='MIT',
     python_requires='>=3.8',
-    install_requires=["pygame"],
-    include_package_data=True
+    install_requires=["pygame"]
 )
